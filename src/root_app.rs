98a6fbd9d5e0ddb8a5f056a4f70cc7f8f17d0c55
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-28 01:01:39 +0300

    Update root_app.rs

--- src/root_app.rs
+++ src/root_app.rs
@@ -14,6 +14,21 @@ impl Default for RootApp {
     }
 }
 
+impl RootApp {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+        Default::default()
+    }
+}
+
+
 impl eframe::App for RootApp {
     fn update( &mut self, ctx: &egui::Context, _frame: &mut eframe::Frame ) {
         egui::CentralPanel::default().show( ctx, |ui| {
