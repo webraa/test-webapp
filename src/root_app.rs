b2782b0ed2c234e599aba050b031061c1a97a7ba
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-28 01:42:04 +0300

    Update root_app.rs

--- src/root_app.rs
+++ src/root_app.rs
@@ -37,6 +37,8 @@ impl eframe::App for RootApp {
         println!("..saved");
     }
 
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update( &mut self, ctx: &egui::Context, _frame: &mut eframe::Frame ) {
         egui::CentralPanel::default().show( ctx, |ui| {
             ui.label("WWWapp Template v1.07");
