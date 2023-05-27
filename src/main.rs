14b6b9fcec0d180589b0368bc0d48ae8df261282
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-27 17:52:38 +0300

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![allow(non_snake_case)]
+
+use eframe::egui;
+
+mod root_app;
+use crate::root_app::*;
+
+
+#[ cfg(not(target_arch = "wasm32")) ]
+fn main() -> Result<(), eframe::Error> {
+    println!("\nStarted..");
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(200., 300.)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "some text 1",
+        options,
+        Box::new( |_cc| Box::<RootApp>::default()),
+   )
+}
+
+
+#[ cfg(target_arch = "wasm32") ]
+fn main() {
+    console_error_panic_hook::set_once();
+
+    tracing_wasm::set_as_global_default();
+
+    let options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "raa_canvas_id",
+            options,
+            Box::new( |_cc| Box::<RootApp>::default()),
+        )
+        .await
+        .expect("failure with starting EFRAME");
+    });
+}
+
