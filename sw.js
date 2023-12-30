f45ac70f2f142f39b49f4a4ef17d3b27371f6ef8
AlexanderARodin <66573649+AlexanderARodin@users.noreply.github.com>
2023-12-30 22:21:21 +0000

    Deploying to gh-pages from @ webraa/test-webapp@467c8afbe572d43d641579f12b86620c443d619f 🚀

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'webapp_template-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './webapp_template.js',
+  './webapp_template_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
